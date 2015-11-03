58624626380a933c2132b60b1fcacc96a2bdcf35
f.diakogiannis <phoevos.diakogiannis@gmail.com>
2015-11-03 11:45:57 +0800

    Update bspline_basis.cpp

--- examples/bspline_basis.cpp
+++ examples/bspline_basis.cpp
@@ -18,12 +18,16 @@ int main(){
 	// Define bspline basis: 
 	bspline_basis mybasis(breakpts,k); 
 
-	// Print all basis for x \in breakpts 
+	// Print all basis for x \in breakpts
+	/* Derivative order of the b-spline basis. der_order <k
+	for der_order = 0, we get the basis $B_i(x)$ 
+	*/
+	int der_order = 0;  
 	for (double x=breakpts.front(); x <= breakpts.back(); x+=0.01)
 		{ 
 		cout << x << " "; 
 		for (auto i=0; i < mybasis.get_nbasis(); ++i)
-			cout << mybasis.get_DjBix(0,i,x)<< " "; // Zeroth order derivative = bspline basis. 
+			cout << mybasis.get_DjBix(der_order,i,x)<< " "; // Zeroth order derivative = bspline basis. 
 		cout << endl; 
 
 
